903636b3c7286c908b0071a4a8727e42829bdde8
fersanz_06 <luisfernandosanchezbriseno052@gmail.com>
2025-11-19 20:27:11 -0600

    Commit 4

--- QuickSort.java
+++ QuickSort.java
@@ -2,7 +2,7 @@ public class QuickSort {
     public static void quickSort(int[] arr, int low, int high) {
         if (low < high) {
             int pi = partition(arr, low, high);
-            quickSort(arr, low, pi);
+            quickSort(arr, low, pi - 1);
             quickSort(arr, pi + 1, high);
         }
     }
@@ -11,7 +11,7 @@ public class QuickSort {
         int pivot = arr[high];
         int i = low - 1;
         for (int j = low; j <= high - 1; j++) {
-            if (arr[j] <= pivot) {
+            if (arr[j] >= pivot) {
                 i++;
                 swap(arr, i, j);
             }
